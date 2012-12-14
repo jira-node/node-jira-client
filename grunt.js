e9c9f84a940656c21e18dfe875447d760d9c5ef0
Chris Moultrie <cmoultrie@endgames.us>
2012-12-14 10:26:25 -0500

    Now linting (passing). Updated docs

--- grunt.js
+++ grunt.js
@@ -8,13 +8,38 @@ module.exports = function(grunt) {
         app: {
             src: ['lib/*.js']
         }
-    }
-    
+    },
+    lint: {
+      files: ['grunt.js', 'lib/**/*.js']
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        browser: true,
+        predef: ['module', 'require', 'console', 'exports']
+      },
+      gruntfile: {
+        src: 'grunt.js'
+      },
+      lib_test: {
+        src: ['lib/**/*.js']
+      }
+    }    
   });
 
   // Default task.
-  grunt.registerTask('default', 'docco');
-  grunt.registerTask('prepare', 'docco bump');
+  grunt.registerTask('default', 'lint docco');
+  grunt.registerTask('prepare', 'default bump');
 
   grunt.loadNpmTasks('grunt-docco');
   grunt.loadNpmTasks('grunt-bump');
