b92aea58c7f0b300feba77dc21daf24748cf3517
Chris Moultrie <tebriel@gmail.com>
2013-01-20 13:47:46 -0500

    Now unit testing 90% of the code

--- grunt.js
+++ grunt.js
@@ -4,6 +4,17 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: '<json:package.json>',
+    jasmine_node: {
+        projectRoot: "./spec",
+        forceExit: false,
+        extensions: 'coffee',
+        jUnit: {
+            report: false,
+            savePath : "./build/reports/jasmine/",
+            useDotNotation: true,
+            consolidate: true
+        }
+    },
     docco: {
         app: {
             src: ['lib/*.js']
@@ -41,6 +52,7 @@ module.exports = function(grunt) {
   grunt.registerTask('default', 'lint docco');
   grunt.registerTask('prepare', 'default bump');
 
+  grunt.loadNpmTasks('grunt-jasmine-node');
   grunt.loadNpmTasks('grunt-docco');
   grunt.loadNpmTasks('grunt-bump');
 };
