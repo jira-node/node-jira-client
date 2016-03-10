c1661f074ba81421c549632018d4ed42299cde3c
Gabriel Baker <me@g403.co>
2016-03-10 07:30:08 +0000

    Fix tests

--- test/jira-tests.js
+++ test/jira-tests.js
@@ -358,7 +358,7 @@ describe('Jira API Tests', () => {
     it('getSprintIssues hits proper url', async () => {
       const result = await dummyURLCall('getSprintIssues', ['someRapidView', 'someSprintId']);
       result.should
-        .eql('http://jira.somehost.com:8080/rest/api/2.0/rapid/charts/sprintreport?rapidViewId=someRapidView&sprintId=someSprintId');
+        .eql('http://jira.somehost.com:8080/rest/greenhopper/1.0/rapid/charts/sprintreport?rapidViewId=someRapidView&sprintId=someSprintId');
     });
 
     it('addIssueToSprint hits proper url', async () => {
